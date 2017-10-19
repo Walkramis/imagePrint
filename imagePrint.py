5287877e30af8e6510ba89434cf08c3f4137fe14
Andreas Höglind <andreas.hoglind@gmail.com>
2017-10-19 15:14:30 +0200

    Almost working refactoring

--- imagePrint.py
+++ imagePrint.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
-
+from random import randint
+import math
 
 class Point:
     def __init__(self, x, y, color):
@@ -13,37 +14,34 @@ class Point:
 class Math:
 
     def flerp(a, b, t):
-        x = a + (b - a)*t
-        return x
+        return a + (b - a)*t
 
     def lerpColors(col1, col2, t):
         x = []
-        for i in range(0,3):
+        for i in range(0, len(col1)):
             y=Math.flerp(col1[i], col2[i], t)
             x.append(y)
-        z = (int(round(x[0])),int(round(x[1])), int(round(x[2])))
-        return z
+        return int(round(x[0])),int(round(x[1])), int(round(x[2]))
 
     def lerpVect(v1, v2, length):
-        a = int(round(v1[0] + (v2[0]-v1[0])*length))
-        b = int(round(v1[1] + (v2[1]-v1[1])*length))
-        v = [a,b]
-        return v
+        a = int(round(p1.x + (p2.x-p1.x)*length))
+        b = int(round(p1.y + (p2.y-p1.y)*length))
+        return [a,b]
 
     def sortVec(v1, v2, v3):
-        x = [v1[1],v2[1],v3[1]]
+        x = [p1.y,p2.y,p3.y]
         x = sorted(x)
 
-        if v2[1] == x[1]:
+        if p2.y == x[1]:
             temp=v1
             v1=v2
             v2=temp
 
-        if v3[1] == x[1]:
+        if p3.y == x[1]:
             temp=v1
             v1=v3
             v3=temp
-        if v2[1] == x[0]:
+        if p2.y == x[0]:
             temp = v2
             v2=v3
             v3=temp
@@ -58,24 +56,30 @@ class canvas:
         self.backingImage =Image.new('RGB', size, (255, 255, 255))
         self.drawer = ImageDraw.Draw(self.backingImage)
  
-    def setPixel(self, x, y, color):
-        self.drawer.point((x, y), fill=color)
+    def setPixel(self, a):
+        #print(a.x, a.y)
+        self.drawer.point((int(round(a.x)), int(round(a.y))), fill=a.color)
 
-    def drawLine(self, point1, point2, color1, color2):
-        xLength = point1[0] - point2[0]
-        yLength = point1[1] - point2[1]
-        xLength = abs(xLength)
+    def drawLine(self, p1, p2):
+        longest_axis = math.ceil(max(abs(p2.x - p1.x), abs(p2.y - p1.y)))
+        for i in range(0, int(longest_axis)):
+            point = Point.lerp(p1, p2, i / int(longest_axis))
+            self.setPixel(point)
 
-        yLength = abs(yLength)
+ #   def drawLine(self, p1, p2):
+        xLength = abs(p1.x - p2.x)
+        yLength = abs(p1.y - p2.y)
 
+        maxLengthRounded = int(round(max(xLength, yLength)))
         maxLength = max(xLength, yLength)
+        #print(p1.x, p2.x, maxLength, xLength)
         xParam = 1
         yParam = 1
         xRatio = 1
         yRatio = 1
-        if point1[0] > point2[0]:
+        if p1.x > p2.x:
             xParam = -1
-        if point1[1] > point2[1]:
+        if p1.y > p2.y:
             yParam = -1        
 
         if max(xLength, yLength) == yLength and yLength != xLength:
@@ -88,19 +92,12 @@ class canvas:
         x=[]
         y=[]
 
-
-        for i in range (0, maxLength +1):
-            x.append(point1[0] + xRatio*i * xParam)
-            x[i]= int(round(x[i]))
-            y.append(point1[1] + yRatio*i * yParam)
-            y[i] = int(round(y[i]))
-            if i != 0:
-                z= i/maxLength
-            else:
-                z=0
-            color = Math.lerpColors(color1, color2, z)
-            #a=[x[i]] [y[i]]
-            self.setPixel(x[i], y[i], color)
+        for i in range(0, maxLengthRounded):
+            if maxLength == 0:
+                maxLength = 1
+            a = Point.lerp(p1,p2, i/maxLength)
+            #print(a.x, p1.x, p2.x)
+            self.setPixel(a)
             
     def drawRectangle(x, y, width, height, col1, col2, col3, col4):
         topLineStart = [x, y]
@@ -113,58 +110,41 @@ class canvas:
             topLineStart[0] += 1
             topLineEnd[0] += 1
 
-    def drawTriangle(self, v1, v2, v3, color, color2, color3):
-        #self.drawLine(v1, v2, symbol)
-        #self.drawLine(v2, v3, symbol)
-        #self.drawLine(v1, v3, symbol)
-        v1, v2, v3 = Math.sortVec(v1, v2, v3)
+    def drawTriangle(self, p1, p2, p3):
+
+        p1, p2, p3 = Math.sortVec(p1, p2, p3)
         
-        spanLengthV3 = abs(v1[1] - v3[1])
-        spanLengthV2 = abs(v1[1] - v2[1])
-        spanLength = abs(max(v1[0], v1[1]))
+        spanLengthV3 = abs(p1.y - p3.y)
+        spanLengthV2 = abs(p1.y - p2.y)
+        spanLength = abs(max(p1.x, p1.y))
         if spanLengthV2 == 0:
             spanLengthV2 = 1
         if spanLengthV3 == 0:
             spanLengthV3 = 1
-        #if max(xLength, yLength) == yLength and yLength != xLength:
-       #     xRatio=xLength/yLength
-        #    yRatio=1
 
-        #if max(xLength, yLength) == xLength and xLength != yLength:
-        #    xRatio=1
-        #    yRatio=yLength/xLength
-        t = abs((v1[1]-v3[1])/(v2[1]-v3[1]))
-        v4 = Math.lerpVect(v3, v2, t)
-        color4 = Math.lerpColors(color3, color2, t)
-        print(color4)
+        t = abs((p1.y-p3.y)/(p2.y-p3.y))
+        p4 = Point.lerp(p3, p2, t)
+
 
         for i in range(0, spanLengthV2 + 1):
-            a = Math.lerpVect(v2,v1, 1/spanLengthV2*i)
-            b = Math.lerpVect(v2,v4, 1/spanLengthV2*i)
-            col1 = Math.lerpColors(color2, color, 1/spanLengthV2*i)
-            col2 = Math.lerpColors(color2, color4, 1/spanLengthV2*i)
-            self.drawLine(a, b, col1, col2)
+            a = Point.lerp(p2,p1, 1/spanLengthV2*i)
+            b = Point.lerp(p2,p4, 1/spanLengthV2*i)
+            self.drawLine(a, b)
 
         for i in range(0, spanLengthV3 + 1):
-            a = Math.lerpVect(v3,v1, 1/spanLengthV3*i)
-            b = Math.lerpVect(v3,v4, 1/spanLengthV3*i)
-            col3 = Math.lerpColors(color3, color, 1/spanLengthV3*i)
-            col4 = Math.lerpColors(color3, color4, 1/spanLengthV3*i)
-            self.drawLine(a, b, col3, col4)
-
-#    def draw(self):
- #       print("-"*self.width*2 + "-")
-  #      string="|" + ('|\n|'.join(' '.join(str(cell) for cell in row) for row in self.a)) + "|"
-   #     print(string)
-    #    print("-"*self.width*2 + "-")
+            a = Point.lerp(p3,p1, 1/spanLengthV3*i)
+            b = Point.lerp(p3,p4, 1/spanLengthV3*i)
+            self.drawLine(a, b)
 
     def saveImage(self, name):
         self.backingImage.save(name + ".png")
 
-screen = canvas(800, 600, " ")
-v1 = [234, 400]
-v2 = [125, 123]
-v3 = [439, 400]
+width = 1920
+height = 1080
+screen = canvas(width, height, " ")
+v1 = [randint(0, width), randint(0,height)]
+v2 = [randint(0, width), randint(0,height)]
+v3 = [randint(0, width), randint(0,height)]
 
 color1 = (255, 125, 79)
 color2 = (14, 255, 124)
@@ -172,18 +152,17 @@ color3 = (67, 28, 255)
 color4 = (25, 90, 100)
 
 
-p1 = Point(245, 576, color1)
-p2 = Point(462, 124, color2)
-p3 = Point(412, 241, color3)
-#p3 = Point.lerp(p1, p2, 0.5)
-#drawLine(p2, p3)
-screen.drawTriangle(v1, v2, v3, color1, color2, color3)
+p1 = Point(v1[0], v1[1], color1)
+p2 = Point(v2[0], v2[1], color2)
+p3 = Point(v3[0], v3[1], color3)
+
+#screen.drawLine(p2, p3)
+screen.drawTriangle(p1, p2, p3)
 #screen.drawTriangle(v1, v2, v3, color1, color2, color3)
 #screen.drawLine(v1, v2, color1, color1)
 #screen.drawLine(v1, v3, color1, color1)
 #screen.drawLine(v2, v3, color1, color1)
-width = 800
-height = 600
-#screen.drawRectangle(v1[0], v1[1], height, width, color1, color2, color3, color4)
+#width = 800
+#height = 600
+#screen.drawRectangle(p1.x, p1.y, height, width, color1, color2, color3, color4)
 screen.saveImage("beautiful")
-#screen.draw()
