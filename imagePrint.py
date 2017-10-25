a0ff84a088f0a1142fc8a5579ae58665eb117bd0
Andreas Höglind <andreas.hoglind@gmail.com>
2017-10-25 15:48:45 +0200

    Changed coordinates to vec3

--- imagePrint.py
+++ imagePrint.py
@@ -3,17 +3,16 @@ from random import randint
 import math
 
 class Point:
-    def __init__(self, v, color):
-        self.x = v[0]
-        self.y = v[1]
-        self.z = v[2]
+    def __init__(self, coordinate, color):
+        self.coordinate = coordinate
         self.color = color
      
     def lerp(a, b, t):
-        return Point([Math.flerp(a.x, b.x, t), Math.flerp(a.y, b.y, t), Math.flerp(a.z, b.z, t)], Math.lerpColors(a.color, b.color, t))
+        return Point(vec3(Math.flerp(a.coordinate.x, b.coordinate.x, t), Math.flerp(a.coordinate.y, b.coordinate.y, t), Math.flerp(a.coordinate.z, b.coordinate.z, t)), Math.lerpColors(a.color, b.color, t))
 
     def copy (a):
-        b = Point([a.x, a.y, a.z], a.color)
+        vec=a.coordinate.copy()
+        b = Point(vec, a.color)
         return b
 
 class Math:
@@ -28,29 +27,46 @@ class Math:
             x.append(y)
         return int(round(x[0])),int(round(x[1])), int(round(x[2]))
 
-    def lerpVect(v1, v2, length):
-        a = int(round(p1.x + (point2.x-p1.x)*length))
-        b = int(round(p1.y + (point2.y-p1.y)*length))
-        return [a,b]
-
     def sortVec(v1, v2, v3):
-        x = [v1.y,v2.y,v3.y]
+        x = [v1.coordinate.y,v2.coordinate.y,v3.coordinate.y]
         x = sorted(x)
 
-        if v2.y == x[1]:
-            temp=v1
+        if v2.coordinate.y == x[1]:
+            temp=v1.copy()
             v1=v2
             v2=temp
 
-        if v3.y == x[1]:
-            temp=v1
+        if v3.coordinate.y == x[1]:
+            temp=v1.copy()
             v1=v3
             v3=temp
-        if v2.y == x[0]:
-            temp = v2
+        if v2.coordinate.y == x[0]:
+            temp = v2.copy()
             v2=v3
             v3=temp
         return v1, v2, v3
+
+class vec3:
+    def __init__(self, x, y, z):
+        self.x=x
+        self.y=y
+        self.z=z
+
+    def copy(a):
+        b=vec3(a.x, a.y,a.z)
+        return b
+
+    def __mul__(self, other):
+        x=self.x*other.x
+        y=self.y*other.y
+        z=self.z*other.z
+        return vec3(x, y, z)
+
+    def __add__(self, other):
+        x=self.x+other.x
+        y=self.y+other.y
+        z=self.z+other.z
+        return vec3(x, y, z)
 
 class Obj:
     def __init__(self, filename):
@@ -68,18 +84,14 @@ class Obj:
         for i in range(0, len(content)):
             if content[i].startswith('v ') == True:
                 v.append(content[i])
+            if content[i].startswith('f'):
+                f.append(content[i])
 
         for i in range(0, len(v)):
             v[i]=v[i][2:]
             vec.append(list(map(float, v[i].split())))
 
-        for i in range(0, len(content)):
-            if content[i].startswith('f'):
-                f.append(content[i])
 
-
-        for i in range(0, len(f)):
-            f[i]=f[i][2:]
 
         fv=[]
         ft=[]
@@ -91,37 +103,36 @@ class Obj:
         d = []
 
         for i in range(0, len(f)):
+            f[i]=f[i][2:]
             y.append(list(f[i].split()))
             a.append(list(y[i][0].split("/")))
             b.append(list(y[i][1].split("/")))
             c.append(list(y[i][2].split("/")))
             d.append(list(y[i][0].split("/")))
 
-
-        for i in range(0, len(f)):
             d[i][0]=int(a[i][0]) - 1
             d[i][1]=int(b[i][0]) - 1
             d[i][2]=int(c[i][0]) - 1
             fv.append(tuple(d[i]))
 
-        if a[0][1] != '':
-            for i in range(0, len(f)):
+            if a[0][1] != '':
                 d[i][0]=int(a[i][1]) - 1
                 d[i][1]=int(b[i][1]) - 1
                 d[i][2]=int(c[i][1]) - 1
                 ft.append(tuple(d[i]))
 
-        for i in range(0, len(f)):
             d[i][0]=int(a[i][2]) - 1
             d[i][1]=int(b[i][2]) - 1
             d[i][2]=int(c[i][2]) - 1
             fn.append(tuple(d[i]))
 
+
         color2 = [200, 200, 200]
         color1 = [100, 100, 100]
         objPoint=[]
-        for i in range(0, len(vec)):
-            objPoint.append(Point(vec[i], color1))
+
+        for i in range(0, len(vec)): 
+            objPoint.append(Point(vec3(vec[i][0], vec[i][1], vec[i][2]), color1))
 
 
         self.points = objPoint
@@ -137,21 +148,6 @@ class Model:
         self.edges = edges
         self.triangles = triangles
 
-        # for i in range(0, len(self.edges)):
-        #     pointA = self.points[self.edges[i][0]].copy()
-        #     pointB = self.points[self.edges[i][1]].copy()
-        #     pointA=canvas.perspective(pointA)
-        #     pointB=canvas.perspective(pointB)
-        #     canvas.drawLine(pointA, pointB)
-
-    # def edges():
-    #     pass
-
-
-    # def triangles():
-    #     pass
-
-
 class canvas:
     def __init__(self, width, height):
         self.width = width
@@ -163,93 +159,49 @@ class canvas:
         drawDistance = 1000
         self.zBuffer = [[drawDistance for x in range(width)] for y in range(height)]
  
-    def drawModel(self, m, model_translate, model_scale, view_translate, wireframeMode):
+    def drawModel(self, m, model_translate, model_scale, view_translate, drawFunction):
 
 
 
-        if wireframeMode == True:
-            for i in range(0, len(m.triangles)):
-                pointA = m.points[m.triangles[i][0]].copy()
-                pointB = m.points[m.triangles[i][1]].copy()
-                pointC = m.points[m.triangles[i][2]].copy()
+        for i in range(0, len(m.triangles)):
+            
+            pointA = m.points[m.triangles[i][0]].copy()
+            pointB = m.points[m.triangles[i][1]].copy()
+            pointC = m.points[m.triangles[i][2]].copy()
 
-                pointA.x*=model_scale[0]
-                pointB.x*=model_scale[0]
-                pointC.x*=model_scale[0]
-                pointA.y*=model_scale[1]
-                pointB.y*=model_scale[1]
-                pointC.y*=model_scale[1]
-                pointA.z*=model_scale[2]
-                pointB.z*=model_scale[2]
-                pointC.z*=model_scale[2]
-                
-                pointA.x+= model_translate[0] + view_translate[0]
-                pointB.x+= model_translate[0] + view_translate[0]
-                pointC.x+= model_translate[0] + view_translate[0]
+            pointA.coordinate=pointA.coordinate*model_scale
+            pointB.coordinate=pointB.coordinate*model_scale
+            pointC.coordinate=pointC.coordinate*model_scale
+  
+            translation = model_translate + view_translate
 
-                pointA.y+= model_translate[1] + view_translate[1]
-                pointB.y+= model_translate[1] + view_translate[1]
-                pointC.y+= model_translate[1] + view_translate[1]
+            pointA.coordinate = pointA.coordinate + translation
+            pointB.coordinate = pointB.coordinate + translation
+            pointC.coordinate = pointC.coordinate + translation
 
-                pointA.y+= model_translate[2] + view_translate[2]
-                pointB.y+= model_translate[2] + view_translate[2]
-                pointC.y+= model_translate[2] + view_translate[2]
+            pointA=self.perspective(pointA)
+            pointB=self.perspective(pointB)
+            pointC=self.perspective(pointC)
+            drawFunction(pointA, pointB, pointC)
 
+    def wireframe(self, pointA, pointB, pointC):
+        self.drawLine(pointA, pointB)
+        self.drawLine(pointB, pointC)
+        self.drawLine(pointA, pointA)
 
-                pointA=self.perspective(pointA)
-                pointB=self.perspective(pointB)
-                pointC=self.perspective(pointC)
-                self.drawLine(pointA, pointB)
-                self.drawLine(pointB, pointC)
-                self.drawLine(pointC, pointA)
-
-        else:
-            for i in range(0, len(m.triangles)):
-
-                pointA = m.points[m.triangles[i][0]].copy()
-                pointB = m.points[m.triangles[i][1]].copy()
-                pointC = m.points[m.triangles[i][2]].copy()
-
-                pointA.x*=model_scale[0]
-                pointB.x*=model_scale[0]
-                pointC.x*=model_scale[0]
-                pointA.y*=model_scale[1]
-                pointB.y*=model_scale[1]
-                pointC.y*=model_scale[1]
-                pointA.z*=model_scale[2]
-                pointB.z*=model_scale[2]
-                pointC.z*=model_scale[2]
-
-                pointA.x+= model_translate[0] + view_translate[0]
-                pointB.x+= model_translate[0] + view_translate[0]
-                pointC.x+= model_translate[0] + view_translate[0]
-
-                pointA.y+= model_translate[1] + view_translate[1]
-                pointB.y+= model_translate[1] + view_translate[1]
-                pointC.y+= model_translate[1] + view_translate[1]
-
-                pointA.z+= model_translate[2] + view_translate[2]
-                pointB.z+= model_translate[2] + view_translate[2]
-                pointC.z+= model_translate[2] + view_translate[2]
-
-                pointA=self.perspective(pointA)
-                pointB=self.perspective(pointB)
-                pointC=self.perspective(pointC)
-                self.drawTriangle(pointA, pointB, pointC)
-
-    def perspective(self, p):
+    def perspective(self, point):
         mp = (self.width/2, self.height/2)
-
-        p.x = p.x + ((mp[0] - p.x)*p.z*0.0005)
-        p.y = p.y + ((mp[1] - p.y)*p.z*0.0005)
+        p=point.copy()
+        p.coordinate.x = p.coordinate.x + ((mp[0] - p.coordinate.x)*p.coordinate.z*0.0005)
+        p.coordinate.y = p.coordinate.y + ((mp[1] - p.coordinate.y)*p.coordinate.z*0.0005)
 
         return p
 
     def setPixel(self, a):
         
-        x=int(round(a.x))
-        y=int(round(a.y))
-        z=int(round(a.z))
+        x=int(round(a.coordinate.x))
+        y=int(round(a.coordinate.y))
+        z=int(round(a.coordinate.z))
         color = (300-z, 300-z, 300-z)
         if self.zBuffer[x][y] > z:
             self.zBuffer[x][y] = z
@@ -258,7 +210,7 @@ class canvas:
 
     def drawLine(self, p1, p2):
 
-        longest_axis = (max(abs(p2.x - p1.x), abs(p2.y - p1.y)))
+        longest_axis = (max(abs(p2.coordinate.x - p1.coordinate.x), abs(p2.coordinate.y - p1.coordinate.y)))
         
         if longest_axis == 0:
             longest_axis = 1
@@ -282,13 +234,13 @@ class canvas:
 
         p1, p2, p3 = Math.sortVec(p1, p2, p3)
 
-        spanLengthV3 = abs(p1.y - p3.y)
-        spanLengthV2 = abs(p1.y - p2.y)
+        spanLengthV3 = abs(p1.coordinate.y - p3.coordinate.y)
+        spanLengthV2 = abs(p1.coordinate.y - p2.coordinate.y)
 
         t=0
-        if (p2.y-p3.y) != 0:
+        if (p2.coordinate.y-p3.coordinate.y) != 0:
 
-            t = abs((p1.y-p3.y)/(p2.y-p3.y))
+            t = abs((p1.coordinate.y-p3.coordinate.y)/(p2.coordinate.y-p3.coordinate.y))
 
         p4 = Point.lerp(p3, p2, t)
         spanV3 = math.ceil(spanLengthV3)
@@ -319,37 +271,22 @@ class canvas:
 width = 1000
 height = 1000
 screen = canvas(width, height)
-# color1 = (randint(0, rgb), randint(0,rgb), randint(0,rgb))
-# color2 = (randint(0, rgb), randint(0,rgb), randint(0,rgb))
-# color3 = (randint(0, rgb), randint(0,rgb), randint(0,rgb))
-# color4 = (25, 90, 100)
 
-hjul1 = [0,600,0]
-hjul2 = [400,600,00]
-hjul3 = [0,600,100]
-hjul4 = [400,600,100]
+hjul1 = vec3(0,200,0)
+hjul2 = vec3(200,200,00)
+hjul3 = vec3(0,200,100)
+hjul4 = vec3(200,200,100)
 
-vTranslate =[0, -200, 0]
+vTranslate = vec3(0, 0, 0)
 
-mScale = [0.25, 0.25, 0.25]
+mScale = vec3(0.25, 0.25, 0.25)
 
-loadedObject = Obj("cylinder")
+loadedObject = Obj("hjul2")
 cube = Model(loadedObject.points, loadedObject.edges, loadedObject.triangles)
-screen.drawModel(cube, hjul1, mScale, vTranslate, False)
-screen.drawModel(cube, hjul2, mScale, vTranslate, False)
-screen.drawModel(cube, hjul3, mScale, vTranslate, False)
-screen.drawModel(cube, hjul4, mScale, vTranslate, False)
-# mTranslate = [0,0,0]
-# loadedObject = Obj("Kub")
-# cube = Model(loadedObject.points, loadedObject.edges, loadedObject.triangles)
-# screen.drawModel(cube, mTranslate, mScale, vTranslate, False)
+screen.drawModel(cube, hjul1, mScale, vTranslate, screen.drawTriangle)
+screen.drawModel(cube, hjul2, mScale, vTranslate, screen.drawTriangle)
+screen.drawModel(cube, hjul3, mScale, vTranslate, screen.drawTriangle)
+screen.drawModel(cube, hjul4, mScale, vTranslate, screen.drawTriangle)
 
-# loadedObject = Obj("cylinder")
-# cube = Model(loadedObject.points, loadedObject.edges, loadedObject.triangles)
-# screen.drawModel(cube, mTranslate, mScale, vTranslate, False)
-
-# loadedObject = Obj("hjul2")
-# cube = Model(loadedObject.points, loadedObject.edges, loadedObject.triangles)
-# screen.drawModel(cube, True)
 
 screen.saveImage("beautiful")
